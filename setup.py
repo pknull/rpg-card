1a808ea0ea504af07dac61d839f647abe6d957d0
BuildTools <louis.grenzebach@gmail.com>
2017-05-26 14:50:38 -0700

    Polish

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'name': 'card_picker',
-    'version': '0.1',
-    'description': 'Card dealing system for use in RPG bots',
-    'author': 'Louis Grenzebach',
-    'author_email': 'louis.grenzebach@gmail.com',
-    'url': 'https://github.com/pknull/rpg-card',
-    'packages': [
+setup(
+    name='card_picker',
+    version=0.1,
+    description='Card dealing system for use in RPG bots',
+    author='Louis Grenzebach',
+    author_email='louis.grenzebach@gmail.com',
+    url='https://github.com/pknull/rpg-card',
+    packages=[
         'card_picker'
     ],
-    'install_requires': ['nose']
-}
-
-setup(**config)
-
+    package_dir={'rpg-card': 'card_picker'},
+    include_package_data=True,
+    zip_safe=False,
+    keywords='rpg card'
+)
